8e5840502d0cfe5a448da9a6d3591d6d6be391a9
Noah Blake <nononoah@gmail.com>
2013-02-17 17:51:33 -0500

    tidying

---
+++ LittlePixel/View Controllers/LPBaseGameplayViewController.h
@@ -0,0 +1,14 @@
+//
+//  LPBaseGameplayViewController.h
+//  LittlePixel
+//
+//  Created by Noah Blake on 2/17/13.
+//  Copyright (c) 2013 Noah Blake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface LPBaseGameplayViewController : UIViewController
+
+@end
---
+++ LittlePixel/View Controllers/LPBaseGameplayViewController.m
@@ -0,0 +1,66 @@
+//
+//  LPBaseGameplayViewController.m
+//  LittlePixel
+//
+//  Created by Noah Blake on 2/17/13.
+//  Copyright (c) 2013 Noah Blake. All rights reserved.
+//
+
+//
+//  LPBaseGameplayViewController.m
+//  LittlePixel
+//
+//  Created by Noah Blake on 2/17/13.
+//  Copyright (c) 2013 Noah Blake. All rights reserved.
+//
+
+#import "LPBaseGameplayViewController.h"
+#import "LPBaseGameplayView.h"
+
+@interface LPBaseGameplayViewController ()
+{
+    CADisplayLink *_displayLink;
+}
+@end
+
+@implementation LPBaseGameplayViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    CGRect tmpFrame = {0, 0, 320, 480};
+    LPBaseGameplayView *tmpLevelOneView = [[LPBaseGameplayView alloc] initWithFrame: tmpFrame];
+    [self.view addSubview: tmpLevelOneView];
+    [tmpLevelOneView release];
+	
+    _displayLink = [CADisplayLink displayLinkWithTarget: tmpLevelOneView selector: @selector(surviving:)];
+	_displayLink.frameInterval = 1;
+    
+	NSRunLoop *loop = [NSRunLoop currentRunLoop];
+	[_displayLink addToRunLoop: loop forMode: NSDefaultRunLoopMode];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) dealloc
+{
+    NSRunLoop *tmpCurrentLoop = [NSRunLoop currentRunLoop];
+	[_displayLink removeFromRunLoop: tmpCurrentLoop forMode: NSDefaultRunLoopMode];
+    [super dealloc];
+}
+
+@end
